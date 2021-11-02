dd95ef75d91535661966cb219db1e4b2b1f5aec6
Abdulazeem2007 <62007047+Abdulazeem2007@users.noreply.github.com>
2021-11-02 17:54:34 +0530

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,7 @@
+import csv
+import plotly.express as px
+
+with open("./data/Size_of_TV_Vs_time_spent.csv") as csv_file:
+    df= csv.DictReader(csv_file)
+    fig= px.scatter(df, x="Size of TV", y="Average time spent watching TV in a week (hours)")
+    fig.show()
